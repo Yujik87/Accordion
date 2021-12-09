b33d8462c5024d6a076cc68f1559752426aa6569
Yujik <yujika87@gmail.com>
2021-12-09 20:10:42 +0300

    Storybook -> Input Inintial

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default {
     title: 'Input'
 };
 
 export const UncontrolledInput = () => <input />
-export const ControlledInput = () => <input />
+
+export const TrackedValueOfUncontrolledInput = () => {
+    const[value, setValue] = useState('')
+    return <><input onChange={(event)=>{
+        const actualValue = event.currentTarget.value;
+        setValue(actualValue);
+    }}/> - {value} </>;
+}
+
+export const GetValueOfUncontrolledInputByButtonPress = () => {
+    const[value, setValue] = useState('')
+    return <><input /><button>save</button> - actual value: {value} </>;
+}
+
+export const ControlledInput = () => <input value={'it-incubator.by'}/>
